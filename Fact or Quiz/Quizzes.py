f9e2e19ef932fad430a5e4c5e5560b5c05c74f42
r-hookham <rowan.hookham@education.nsw.gov.au>
2026-08-18 12:44:54 +1000

    dsadsa

--- Fact or Quiz/Quizzes.py
+++ Fact or Quiz/Quizzes.py
@@ -1,46 +1,40 @@
-english = [
-        true_english = [
+true_english = [
+    
+]
+false_english = [
             
-        ]
-        false_english = [
-            
-        ]
 ]
 
-
-maths = [
-        true_maths = [
+true_maths = [
             
-        ]
+]
+false_maths = [
+    "The first ever person to add 2 numbers together was called 'Addison Maquition'. ",   
+    "The first ever person to discover maths was called 'Matthew E. Matician'.",
+    "The first ever person to subtract 2 numbers was called 'Mine Us E. '.",
 
-        false_maths = [
-            
-        ]
 ]
 
-history = [
-        true_history = [
+true_history = [
             
-        ]
-        false_history = [
-            
-        ]
+]
+false_history = [
+    "When the British founded the United States, David Martinez found green oranges, and decided to call them limes to trick the other British that travelled with him, despite their sweet flavour.",
+
 ]
 
-science = [
-        true_science = [
-        
-        ]
-        false_science = [
+true_science = [
             
-        ]
+]
+false_science = [
+    "Chocolate milk comes from brown cows.",
+    "e = MC squared was though up by a group of four men, called 'Al', 'Bert', 'Ein' and 'Stein', respectively.",
+    ""
 ]
 
-computer = [
-        true_computer = [
+true_computer = [
             
-        ]
-        false_computer = [
-            
-        ]
+]
+false_computer = [
+    "The first ever video game, 'Cyberpunk 2077, was directed, produced, written, programmed, acted, and funded by Shigeru Miyamoto.",
 ]
