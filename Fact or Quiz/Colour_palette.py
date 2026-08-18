98cd600fc368c03b3695951d47458f3a0d9f4815
r-hookham <rowan.hookham@education.nsw.gov.au>
2026-08-18 12:10:04 +1000

    Added colour palettes

--- Fact or Quiz/Colour_palette.py
+++ Fact or Quiz/Colour_palette.py
@@ -1,15 +1,35 @@
 #Colour palette for 'Fact or Quizzes Game'.
 
 #Feel free to change these to whatever hexadecimal colours you would like, after the # sign.
+
+
+#Complete black and white codes are "#000000" and "#FFFFFF"
+#Flipping the hex codes works as a 'dark mode' feature.
+
+#Here is a list of custom colours you can copy into the two lines at the top of this script. 
+#Just remove the '#' symbols from the 2 lines you'd like to use, and add them again to disable them.
+
+#Classic Windows Program
+#foregroundcolour = "#1B1725"
+#backgroundcolour = "#C0C0C0"
+
+#Pink
+#foregroundcolour = "#06111F"
+#backgroundcolour = "#DB7F8E"
+
+#Purple
+#foregroundcolour = "#2C2A2A"
+#backgroundcolour = "#D175FF"
+
+#Lilac
 foregroundcolour = "#1B1725"
 backgroundcolour = "#EED9F1"
 
-#Flipping the hex codes works as a 'dark mode' feature.
-#Complete black and white codes are "#000000" and "#FFFFFF"
+#Light blue
+#foregroundcolour = "#383F51"
+#backgroundcolour = "#B2ABF8"
 
-#Original colours to restore to prior colours. 
-#Copy these and place them above if you'd like the original colour palette.
-
-#foregroundcolour = "#1B1725"
-#backgroundcolour = "#E7E6DE"
+#Red
+#foregroundcolour = "#B3001B"
+#backgroundcolour = "#262626"
 
