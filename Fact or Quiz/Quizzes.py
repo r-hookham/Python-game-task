8feccc97e1e63b8bff10dbbacd09f9c38762631d
r-hookham <rowan.hookham@education.nsw.gov.au>
2026-09-08 14:23:49 +1000

    jkglgjklgkldsadasdsa

--- Fact or Quiz/Quizzes.py
+++ Fact or Quiz/Quizzes.py
@@ -11,8 +11,8 @@ true_english = [
     "The symbol on the 'pound' key (#) is called an octothorpe.",
 ]
 false_english = [
-    "An ellipsis (...) is a way of indicating an increase in volume towards at the end of a sentence.",
-    "Foreshadowing is a literary device that writers utilize as a means to tell the reader about what has happened "
+    "An ellipsis (...) is a way of indicating an increase in volume and pitch towards at the end of a sentence.",
+    "Foreshadowing is a literary device that writers utilize as a means to tell the reader about what has happened, and is often."
     "During Shakespeare's lifetime, he added 1,700 additional words to the English language.",
     "The most common letter in the English language.",
     "Despite it's infrequent use, 'overmorrow' is what 'the day after tomorrow' is called.",
@@ -52,7 +52,7 @@ true_history = [
     "When the Titanic sank, 2228 people were on it. Only 706 survived.",
     "Paper was invented early in the second century by Chinese eunuch.",
     "Carrots used to be purple, white, red and black, and were grown to be orange to be less bitter and more sweet compared to the original colours, and legend has it they were changed tribute to William of Orange.",
-    "People used to be put in barrels as a form of torture." 
+    "People used to be put in barrels as a form of torture."
 ]
 false_history = [
     "When the British founded the United States, David Martinez found green oranges, and decided to call them limes to trick the other British that travelled with him into not eating them, despite their sweet flavour, which they didn't know because they never tasted them.",
@@ -62,43 +62,85 @@ false_history = [
     "In Ancient Rome, there were some female gladiators, called 'gladitri' as singular, and 'gladiclices' plural. There were much less gladiclices compared to the male 'gladiators'.",
     "In the last, at least 7000 years no new animals have been domesticated.",
     "When the Titanic sank, 226 people were on it. Only 16 survived.",
-    "Paper was invented early in the 19th century in Toronto, Canada, by .",
+    "Paper was invented early in the 19th century in Toronto, Canada by Paul Oregon.",
     "Carrots used to be purple, white, dark and light blue, and were grown to be orange to be less bitter and more sweet compared to the original colours, and legend has it they were changed tribute to William of Orange.",
-    "People used wear barrels as a costume." 
+    "People used wear barrels as a costume.",
 
 ]
 
 true_science = [
-            
+    "Apparently the universe began in a massive explosion which was probably deafening, but thankfully, ears didn't exist yet.",
+    "When a wheel rolls, the pressure rolls the entire planet back and away from you, giving you the impression that you're moving forward. Which incredibly, you actually are.",
+    "It takes more calories to eat a piece of celery than the celery has in it to begin with.",
+    "The pancreas produces insulin.",
+    "The human body is comprised of 80% water.",
+    "Giraffes and rats can last longer without water than camels.",
+    "Our eyes are always the same size from birth, but our nose and ears never stop growing.",
+    "A honeybee can fly at 24 kilometers per hour.", 
+    "The average human heart beats over 100,000 times a day. ",
+    "On average, the human brain contains 100 billion nerve cells, or 100,000,000,000.",
+    "Fish that live more than 800 meters below the ocean surface don't have eyes.",
+    "Hair is made from the same substance as fingernails.",
+    "It takes the stomach an hour to break down cows' milk.",
+    "Insects outnumber humans 100,000,000 to one.",
+    "Clownfish, wrasses, moray eels, gobies and other fish species are known to change sex, including reproductive functions. A school of clownfish is always built into a hierarchy with a female fish at the top. When she dies, the most dominant male changes sex and takes her place.",
+    "The common idea that only 10% of the brain is used is not true as it is impossible to determine the actual percentage because of the complexity of the brain.",
+    "Spiders have transparent blood.",            
 ]
 false_science = [
     "Chocolate milk comes from brown cows.",
     "e = MC squared was though up by a group of four men, called 'Al', 'Bert', 'Ein' and 'Stein', respectively.",
+    "Apparently the universe began in a massive explosion which was probably deafening, unfortunately, ears did exist and this probably made all of them deaf.",
+    "It takes less calories to eat a piece of celery than the celery has in it to begin with.",
+    "The pancreas produces urine.",
+    "The human body is comprised of 103% water.",
+    "Giraffes and rats can last longer without water than camels.",
+    "Our eyes are always the same size from birth, but our nose and ears never stop growing.",
+    "A honeybee can fly at 48 kilometers per hour.", 
+    "The average human heart beats over 10 times a day. ",
+    "On average, the human brain contains 100 nerve cells.",
+    "Fish that live more than 5 meters below the ocean surface don't have eyes.",
+    "Hair is made from the same substance as the skin of fingers.",
+    "It takes the stomach an hour to break down cows' milk.",
+    "Insects outnumber humans fourty to one.",
+    "The common idea that only 10% of the brain is used is true as it is very possible to determine the actual percentage because of the complexity of the brain.",
+    "Spiders have no blood.",  
 ]
 
 true_computer = [
-            
-]
-
-false_computer = [
-    "The first ever video game, 'Cyberpunk 2077, was directed, produced, written, programmed, acted, and funded by Shigeru Miyamoto.",
-    "1 byte is equivalent to 4 bits of computer memory. In other words, 256 different options or 2 to the power of 8",
-    "1 kilobyte is equivalent to 512 bytes of computer memory. In other words, 2 to the power of 16,384.",
-    "1 megabyte is equivalent to 2048 kilobytes of computer memory. In other words, 2 to the power of 268,435,456.",
+    "1 byte is equivalent to 8 bits of computer memory. In other words, 256 different options or 2 to the power of 8",
+    "1 kilobyte is equivalent to 1000 bytes of computer memory. In other words, 2 to the power of 8192.",
+    "1 megabyte is equivalent to 1000 kilobytes of computer memory. In other words, 2 to the power of 268,435,456.",
     "Super Mario Bros. was released in 1985 in North America and Japan, and 1987 in Europe and Australia.",
     "The Original 'The Legend of Zelda' released in 1986 in Japan and 1987 in Europe and Australia.",
     "Keyboards with the standard (100%) layout, arrow keys, function keys and numberpad have usually have 104 keys.",
     "Despite DOOM (1993) only being released for MS-DOS, due to having source code released by its developers (id Software), as well as very low hardware requirements, specifically using no GPU rendering, and instead rendering all graphics with the CPU, meant that DOOM (1993) has made a history for being able to be ran on web browsers, calculators, MS Paint and also, strangly, pregnancy tests.",
-    "The Nintendo 3DS family of systems is able to play 3DS, DS, Game Boy Advance and Game Boy cartidges through backwards compatibility.",
-    "'New' 3DS has two additional trigger buttons called 'ZL' and 'ZR', and a secondary 'C Stick', used to control the camera in a selection of games, and the ability to play Wii games that supported classic controller input natively, without any streaming, such as Mario Kart Wii and Super Smash Bros. Brawl.",
+    "The Nintendo 3DS family of systems is able to play 3DS, DS and Game Boy Advance games through backwards compatibility.",
+    "'New' 3DS systems feature an 804mHz CPU with an L2 cache, while the non-'New' models have a 268mHz CPU with only an L1 cache.",
+    "'New' 3DS has two additional trigger buttons called 'ZL' and 'ZR', and a secondary 'C Stick', used to control the camera in a selection of games.",
     "PNG image files use lossless compression, meaning they have higher quality, especially when scaled up, as well as an additional alpha channel, allowing images to have transparency.",
     "A single layer DVD can store 4.7GB, double layer DVD can store 8.5GB, a single layer Blu-ray can store 25GB, and a double layer Blu-ray can store 50GB.",
     "DVDs can output video up to 720x480 pixels, standard Blu-ray can output 1920x1080 pixels, and 4K Blu-ray can output 3840x2160 pixels.",
-     "The third ever video game was 'Tennis for 2' in 1958 to demonstrate at the Brookhaven National Laboratory's annual public exhibition that the Donner Model 30 analog computer could simulate wind resistance trajectories.",
+    "The first ever video game was 'Tennis for 2' in 1958 to demonstrate at the Brookhaven National Laboratory's annual public exhibition that the Donner Model 30 analog computer could simulate wind resistance trajectories.",
     "The average person`s left hand does 56% of the typing.",
-    "The 'save' icon on the Microsoft Office Suite and many other programs shows a floppy disk with the shutter on backwards.",
-    "I'd just like to interject for a moment. What you're refering to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.",
-    "Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called Linux, and many of its users are not aware that it is basically the GNU system, developed by the GNU Project.",
-    "There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called Linux distributions are really distributions of GNU/Linux!",
-    
+    "The 'save' icon on the Microsoft Office Suite and many other programs shows a floppy disk with the shutter on backwards.",      
+]
+false_computer = [
+    "The first ever video game, 'Cyberpunk 2077, was directed, produced, written, programmed, acted, and funded exclusively by Shigeru Miyamoto.",
+    "1 byte is equivalent to 8 bits of computer memory. In other words, 256 different options or 2 to the power of 15",
+    "1 kilobyte is equivalent to 1000 bytes of computer memory. In other words, 2 to the power of 100.",
+    "1 megabyte is equivalent to 1000 kilobytes of computer memory. In other words, 2 to the power of 2000.",
+    "Super Mario Bros. was released in 1976 in North America and Japan, and 1978 in Europe and Australia.",
+    "The Original 'The Legend of Zelda' released in 1530 in Japan and 2005 in Europe and Australia.",
+    "Keyboards with the standard (100%) layout, arrow keys, function keys and numberpad have usually have 64 keys.",
+    "Due to DOOM (1993)'s code being closed source and only being released for MS-DOS as well as very low hardware requirements, specifically using no GPU rendering, and instead rendering all graphics with the CPU, meant that DOOM (1993) could only run on MS-DOS devices.",
+    "The Nintendo 3DS family of systems is able to play 3DS, DS and Game Boy Advance games through backwards compatibility.",
+    "'New' 3DS systems feature an 5tHz CPU with an L2 cache, while the non-'New' models have a 16Hz CPU with only an L1 cache.",
+    "'New' 3DS has two additional trigger buttons called 'ZL' and 'ZR', and a secondary 'C Stick', used to control the camera in a selection of games.",
+    "PNG image files use lossless compression, meaning they have lower quality, especially when scaled up, as well as an additional alpha channel, allowing images to have no transparency.",
+    "A single layer DVD can store 4.7GB, double layer DVD can store 2GB, a single layer Blu-ray can store 50GB, and a double layer Blu-ray can store 100GB.",
+    "DVDs can output video up to 72x48 pixels, standard Blu-ray can output 192x108 pixels, and 4K Blu-ray can output 38400x21600 pixels.",
+    "The first ever video game was 'Tennis for 2' in 1958 to demonstrate at the Brookhaven National Laboratory's annual public exhibition that video games are very, very fun.",
+    "The average person`s left hand does 2% of the typing.",
+    "The 'save' icon on the Microsoft Office Suite and many other programs shows a mysterious relic which no one knows the name of.",
 ]
